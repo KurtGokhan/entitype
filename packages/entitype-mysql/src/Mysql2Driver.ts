dae81c13df082ab6e8564e52d12a8626a0cf0106
Kurt <gokhan.kurt@siemens.com>
2017-10-19 09:04:51 +0300

    driver returns correct type - RowData now

--- packages/entitype-mysql/src/Mysql2Driver.ts
+++ packages/entitype-mysql/src/Mysql2Driver.ts
@@ -1,13 +1,15 @@
 import { ConnectionOptions } from 'entitype';
-import { Driver, DriverAdapter } from 'entitype/dist/plugins';
+import { Driver, DriverAdapter, RowData, ColumnData } from 'entitype/dist/plugins';
 import { createConnection } from 'mysql2/promise';
+import { RowDataPacket } from 'mysql';
 
 @Driver('mysql2')
 export class Mysql2Driver implements DriverAdapter {
-  async runQuery(query: string, options: string | ConnectionOptions): Promise<any> {
+  async runQuery(query: string, options: string | ConnectionOptions): Promise<[RowData[], ColumnData[]]> {
     const connection = await createConnection(<any>options);
 
-    let result = await connection.query(query);
-    return result;
+    let [rows, columns] = await connection.query(query);
+    let rowsData = rows as RowData[];
+    return [rowsData, columns];
   }
 }
