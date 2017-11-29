c2eda0de4ab8c2524607741d7caf6852de910e5b
Gokhan Kurt <krtgokhan@hotmail.com>
2017-11-29 11:56:52 +0300

    added timeout for northwind database, fixed one-to-many test assertion

--- packages/entitype-integration-tests/test/mysql/one-to-many/basic.spec.ts
+++ packages/entitype-integration-tests/test/mysql/one-to-many/basic.spec.ts
@@ -16,6 +16,6 @@ describe('query > one-to-many > basic', async () => {
     expect(details[0].length).to.eql(2);
     expect(details[0][0].quantity).to.eql('100.0000');
     expect(details[0][0].product).not.to.be.null;
-    expect(details[0][0].product.id).to.eql(48);
+    expect(details[0][0].product.id).to.eql(34);
   });
 });
--- packages/entitype-integration-tests/test/northwind/helper.ts
+++ packages/entitype-integration-tests/test/northwind/helper.ts
@@ -1,8 +1,11 @@
 import * as fs from 'fs';
+import { IBeforeAndAfterContext } from 'mocha';
 import * as path from 'path';
+
 import { dropAndCreateDatabase } from '../helper';
 
-export async function integrationTestDatabaseSeed() {
+export async function integrationTestDatabaseSeed(this: IBeforeAndAfterContext) {
+  this.timeout(10000);
   let seed = fs.readFileSync(path.join(__dirname, './seed.sql')).toString();
   await dropAndCreateDatabase(seed);
 }
